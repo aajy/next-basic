029c7b8335ec46529fb1221664ace056f0c25154
aajy <aajy.000@gmail.com>
2024-01-09 17:33:00 +0900

    about 서브페이지 메뉴 active 작업중

--- components/aboutMenu/aboutMenu.jsx
+++ components/aboutMenu/aboutMenu.jsx
@@ -1,13 +1,29 @@
+'use client';
+import styles from './aboutMenu.scss';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 export default function AboutMenu() {
+	const pathname = usePathname();
+	console.log('pathname: ', pathname);
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link href='/about/president'>President</Link>
+					<Link
+						href='/about/president'
+						className={pathname === '/about/president' ? styles.on : ''}
+					>
+						President
+					</Link>
 				</li>
 				<li>
-					<Link href='/about/company'>About</Link>
+					<Link
+						href='/about/company'
+						className={pathname === '/about/company' ? styles.on : ''}
+					>
+						Company
+					</Link>
 				</li>
 			</ul>
 		</nav>
