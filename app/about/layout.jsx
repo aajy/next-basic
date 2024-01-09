5d43bff708d85a83b2da4c8f1861e7176e286e9c
tubi55 <tubi55@nate.com>
2024-01-09 15:58:02 +0900

    서브페이지 전용 레이아웃 컴포넌트 생성

---
+++ app/about/layout.jsx
@@ -0,0 +1,9 @@
+import AboutMenu from '@/components/aboutMenu/aboutMenu';
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <AboutMenu />
+      {children}
+    </div>
+  );
+}
