38e17ce9ab5a17d73d254293e97eb1e77a416de7
tubi55 <tubi55@nate.com>
2024-01-09 12:51:15 +0900

    폴더기반 페이지 라우터 자동 설정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "./styles/variables.scss";`,
   },
 };
 
