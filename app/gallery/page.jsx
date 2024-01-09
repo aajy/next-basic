5a4dba24c17d03f81d7e0a7a85360928968c13a0
tubi55 <tubi55@nate.com>
2024-01-09 13:08:57 +0900

    페이지기반 폴더 라우팅

---
+++ app/gallery/page.jsx
@@ -0,0 +1,9 @@
+import styles from './gallery.module.scss';
+
+export default function Gallery() {
+  return (
+    <main className={styles.gallery}>
+      <h1>Gallery Page</h1>
+    </main>
+  );
+}
