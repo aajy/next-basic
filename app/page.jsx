b73eb280b9a5ab61f1b833c58abb4635840a741c
tubi55 <tubi55@nate.com>
2024-01-10 12:23:36 +0900

    추가 주석

--- app/page.jsx
+++ app/page.jsx
@@ -24,9 +24,10 @@ export default function Home() {
   Next에서 Image컴포넌트를 사용해야 되는 이유
   1. 원본 빌드시에 import한다음에 브라우저 크기에 따른 이미지 용량을 자동 최적화시켜서
   2. 클라이언트 요청이 들어올떄 미리 최적화시킨 이미지를 렌더링
-  3. quality속성으로 직접 최적화가능 0~100(default:75)
-  4. priority속성을 이용해서 빨리 출력해야 되야되는 이미지에 우선순위를 부여가능
-  5. 기본적으로 Image 컴포넌트는 lazy loading기능을 적용하는데 priority 적용시 lazy loading 기능은 강제 해제됨(주의)
+  3. 브라우저 폭을 인지해서 그때마다 폭에 따른 최적화된 이미지 제공
+  4. quality속성으로 직접 최적화가능 0~100(default:75)
+  5. priority속성을 이용해서 빨리 출력해야 되야되는 이미지에 우선순위를 부여가능
+  6. 기본적으로 Image 컴포넌트는 lazy loading기능을 적용하는데 priority 적용시 lazy loading 기능은 강제 해제됨(주의)
 
 
 */
