5343b8f3d79dac60307a5f3f47a8949ab6ebe9ea
aajy <aajy.000@gmail.com>
2024-01-10 14:21:44 +0900

    Next <image> 태그 속성 활용, 이미지 최적화 확인

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,28 @@
+import Image from 'next/image';
 import styles from './main.module.scss';
+import pic from '@/public/pic.jpg';
+import pic2 from '@/public/pic2.jpg';
+import clsx from 'clsx';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<h1>메인페이지</h1>
+			<h1>Main Page</h1>
+
+			<div className={clsx(styles.pic)}>
+				<Image
+					src={pic2}
+					alt='image'
+					quality={75}
+					fill
+					sizes='(min-width: 808px) 50vw, 100vw'
+					placeholder='blur'
+				/>
+			</div>
+
+			<div className={clsx(styles.pic)}>
+				<Image src={pic} alt='image' fill quality={75} placeholder='blur' />
+			</div>
 		</main>
 	);
 }
