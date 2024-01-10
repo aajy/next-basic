46b9827af281d73b114affcae253a80dab0ec4f2
aajy <aajy.000@gmail.com>
2024-01-10 16:24:01 +0900

    서버 컴포넌트에서 데이터 fetching처리

--- app/about/president/page.jsx
+++ app/about/president/page.jsx
@@ -1,7 +1,15 @@
-export default function President() {
+async function fetchPresident() {
+	const data = await fetch('https://jsonplaceholder.typicode.com/users/1');
+	const resultData = await data.json();
+	return resultData;
+}
+export default async function President() {
+	const data = await fetchPresident();
+	console.log('data', data);
 	return (
-		<main className='president'>
-			<h1>President Page</h1>
-		</main>
+		<div className='president'>
+			<h3>President</h3>
+			<p>{data.name}</p>
+		</div>
 	);
 }
