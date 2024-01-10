0b99422d3e581a8de7ead4025a5ce739048e9919
tubi55 <tubi55@nate.com>
2024-01-10 11:56:10 +0900

    이미지 최적화 기능

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,15 @@
 import styles from './main.module.scss';
+import pic from './pic.jpg';
+import clsx from 'clsx';
+console.log(pic);
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Main Page</h1>
+      <div className={clsx(styles.pic)}>
+        <img src={pic.src} alt="image" />
+      </div>
     </main>
   );
 }
-
-/*
-Brendan Eich (Javascript)
-
-Ryan Dahl (Node, Type)
-
-React (Facebook ---> meta)
-
-Next (Vercel)
-
-Nescape --> Explore(MS) --> FireFox(Mozilla) --> Chrome (Chrome)
-
-W3C vs MDN
-
-*/
