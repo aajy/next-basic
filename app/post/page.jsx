f674a80fc6520742bab8599475f27ba77197397d
tubi55 <tubi55@nate.com>
2024-01-11 11:53:59 +0900

    데이터 요청, 응답 흐름

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -5,7 +5,9 @@ export default function Post() {
   const [Post, setPost] = useState([]);
 
   useEffect(() => {
+    //순서3 -컴포넌트 마운트시 미리 서버라우터로 데이터요청
     fetch('/api/requestPost')
+      //순서4-응답이 성공하면 응답객체 서버로부터 전달받고 state에 전달
       .then((data) => data.json())
       .then((json) => {
         console.log(json);
@@ -15,6 +17,7 @@ export default function Post() {
 
   return (
     <div className="postList">
+      {/* 순서5- 응답받은 State값으로 화면 렌더링 */}
       {Post.map((post) => {
         return (
           <article key={post.name}>
