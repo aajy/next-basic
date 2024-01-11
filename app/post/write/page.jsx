0cbbffca1eeab0fd395e71464afa50b9ef11909d
aajy <aajy.000@gmail.com>
2024-01-11 16:39:10 +0900

    글 작성페이지에서 글 저장요청보내고 전달한 정보값을 parsing한 뒤 성공응답객체를 다시 클라이언트로 전달해서 확인.

---
+++ app/post/write/page.jsx
@@ -0,0 +1,58 @@
+'use client';
+import { useState } from 'react';
+import styles from './write.module.scss';
+import clsx from 'clsx';
+
+export default function Write() {
+	const [Post, setPost] = useState({ title: '', content: '' });
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setPost({ ...Post, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		fetch('/api/requestPost', {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(Post),
+		}).then((response) => {
+			if (response.ok) {
+				response.json().then((data) => {
+					console.log(data);
+					alert('글 저장에 성공했습니다.');
+				});
+			} else {
+				console.log(response);
+				alert('글 저장에 실패했습니다.');
+			}
+		});
+	};
+
+	return (
+		<div className={clsx(styles.write)}>
+			<h1>Write Post</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='title'
+					name='title'
+					value={Post.title}
+					onChange={handleChange}
+				/>
+				<textarea
+					name='content'
+					cols='30'
+					rows='3'
+					valut={Post.content}
+					onChange={handleChange}
+				></textarea>
+
+				<button>Send Post</button>
+			</form>
+		</div>
+	);
+}
