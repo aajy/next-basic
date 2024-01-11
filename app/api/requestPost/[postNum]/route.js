c0795544fbec4a6c208616500c70a8d1804a2c25
aajy <aajy.000@gmail.com>
2024-01-11 17:49:33 +0900

    post list보여주는 페이지에서 삭제버튼 클릭 시  삭제기능 적용

--- app/api/requestPost/[postNum]/route.js
+++ app/api/requestPost/[postNum]/route.js
@@ -1,8 +1,26 @@
 import { postDB } from '@/app/DB/postData';
 import { NextResponse } from 'next/server';
+import fs from 'fs';
 
 export function GET(req, res) {
 	console.log(req.url);
 	const { postNum } = res.params;
 	return NextResponse.json({ result: postDB[parseInt(postNum)] });
+}
+export function DELETE(req, res) {
+	const { postNum } = res.params;
+	if (postNum) {
+		const updatedPostDB = postDB.filter((el) => el.id !== parseInt(postNum));
+		const combinedData = JSON.stringify(updatedPostDB);
+		console.log('combinedData: ', combinedData);
+		//fs모듈로 기존 postData.js의 파일내용을 변경해서 저장
+		fs.writeFileSync(
+			'./app/DB/postData.js',
+			`export const postDB=${combinedData};`,
+			'utf-8'
+		);
+		return NextResponse.json({ result: updatedPostDB }, { status: 200 });
+	} else {
+		return NextResponse.json({ result: 'fail to Post' }, { status: 400 });
+	}
 }
--- app/post/page.jsx
+++ app/post/page.jsx
@@ -6,7 +6,24 @@ import clsx from 'clsx';
 
 export default function Post() {
 	const [Post, setPost] = useState([]);
-
+	const handleDelete = (id) => {
+		console.log('id: ', id);
+		fetch(`/api/requestPost/${id}`, {
+			method: 'DELETE',
+			headers: { 'Content-type': 'application/json' },
+		}).then((response) => {
+			if (response.ok) {
+				response.json().then((data) => {
+					console.log(data);
+					setPost(data.result);
+					alert('글 삭제에 성공했습니다.');
+				});
+			} else {
+				console.log(response);
+				alert('글 삭제에 실패했습니다.');
+			}
+		});
+	};
 	useEffect(() => {
 		fetch('/api/requestPost')
 			.then((data) => data.json())
@@ -27,6 +44,7 @@ export default function Post() {
 						<h2>
 							<Link href={`/post/${post.id}`}>{post.title}</Link>
 						</h2>
+						<button onClick={() => handleDelete(post.id)}>delete</button>
 					</article>
 				);
 			})}
