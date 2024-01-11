0f49676e26296eab62807571aec6f0ea0e80440f
aajy <aajy.000@gmail.com>
2024-01-11 17:10:02 +0900

    글 저장 내역으로 파일을 새로 덮어쓰기 해서 영구적으로 데이터 연동->app/DB/postData.js에서 확인.

--- DB/postData.js
+++
@@ -1,5 +0,0 @@
-export const postDB = [
-	{ id: 0, title: 'title1', content: 'First Content comes here in detail' },
-	{ id: 1, title: 'title2', content: 'Second Content comes here in detail' },
-	{ id: 2, title: 'title3', content: 'Third Content comes here in detail' },
-];
